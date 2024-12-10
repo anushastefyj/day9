67b93b7cc87d295df07835734950633a389042de
anushastefyj <24hr5a3001@mtieat.org>
2024-12-10 14:13:18 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class foreach {
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+         String a[]={"apple","orange","banana"};
+          for(String b:a)
+          
+          System.out.print(b+",");
+    }
+}

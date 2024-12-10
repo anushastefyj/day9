b34a158bd3a379852e304e6a51da5ece3d13a95a
anushastefyj <24hr5a3001@mtieat.org>
2024-12-10 10:37:10 +0530

    Add files via upload

---
+++ oddstring.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class oddstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        String result = "";
+        for (int i = 1; i < input.length(); i += 2) {
+            result += input.charAt(i);
+        }
+        System.out.println("Result: " + result);
+    }
+}
